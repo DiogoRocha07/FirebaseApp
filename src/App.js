73fa5729c540dd696fb57c9f8d5b1a9b484fe6f0
Diogo <diogorocha0712@gmail.com>
2023-08-29 15:38:36 -0300

    Com cadastrado e permanencia de login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { db } from "./firebaseConnection";
+import { db, auth } from "./firebaseConnection";
+
 import "./app.css";
 import {
   doc,
@@ -13,10 +14,23 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
 function App() {
   const [titulo, setTitulo] = useState("");
   const [autor, setAutor] = useState("");
   const [idPost, setIdPost] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const [user, setUser] = useState(false);
+  const [userDetail, setUserDetail] = useState({});
 
   const [posts, setPosts] = useState([]);
 
@@ -39,6 +53,27 @@ function App() {
 
     loadPosts();
   }, []);
+
+  useEffect(() => {
+    async function checkLogin(){
+     onAuthStateChanged(auth, (user) =>{
+      if(user){
+        // se tem usuario logado ele entra aqui....
+        setUser(true)
+        setUserDetail({
+          uid: user.uid,
+          email: user.email,
+        })
+      }else{
+        //não possui nenhum user logado
+        setUser(false)
+        setUserDetail({})
+      }
+     }) 
+    }
+
+    checkLogin()
+  }, [])
 
   //adicionando um novo post com id unico
   async function handleAdd() {
@@ -128,11 +163,94 @@ function App() {
       });
   }
 
+  async function novoUsuario() {
+    await createUserWithEmailAndPassword(auth, email, senha)
+      .then(() => {
+        console.log("cadastrado");
+
+        setEmail("");
+        setSenha("");
+      })
+      .catch((error) => {
+        if (error.code === "auth/weak-password") {
+          alert("senha muito fraca.");
+        } else if (error.code === "auth/email-already-in-use") {
+          alert("Email já existe");
+        }
+      });
+  }
+  async function logarUsuario() {
+    await signInWithEmailAndPassword(auth, email, senha)
+      .then((value) => {
+        console.log("feito");
+
+        setUserDetail({
+          uid: value.user.uid,
+          email: value.user.email,
+        });
+        setUser(true);
+
+        setEmail("");
+        setSenha("");
+      })
+      .catch(() => {
+        alert("Erro");
+      });
+  }
+
+  async function logout() {
+    await signOut(auth)
+    .then(()=> {
+      setUser(false)
+      setUserDetail({})
+    })
+  }
+
   return (
     <div className="App">
       <h1>ReactJs + Firebase</h1>
 
+      {user && (
+        <div>
+          <strong>Seja bem-vindo(a) (Você está logado!)</strong> <br />
+          <span>
+            ID: {userDetail.uid} - Email: {userDetail.email}
+          </span>{" "}
+          <br />
+          <button onClick={logout}>Logout</button>
+          <br /> <br />
+        </div>
+      )}
+
       <div className="container">
+        <h2>Usuarios</h2>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Digite um Email"
+        />
+        <br />
+        <label>Senha</label>
+        <input
+          type="password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="Digite sua senha"
+        />
+        <br />
+        <button onClick={novoUsuario}>Cadastrar</button> <br />
+        <button onClick={logarUsuario}>Efetuar login</button>
+      </div>
+
+      <br />
+      <hr />
+
+      <div className="container">
+        <h2>
+          <strong>POSTS</strong>
+        </h2>
         <label>Id do Post:</label>
         <input
           placeholder="Digite o Id do Post"
